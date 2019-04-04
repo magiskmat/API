aa6dc1b663db99fe284515e1ea0ec8b813be6e96
magiskmat <johannahakansson87@hotmail.com>
2019-04-04 10:49:08 +0200

    kap 02_02

--- johanna/src/Components/News/News.js
+++ johanna/src/Components/News/News.js
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import NewSingle from './NewSingle';
 
 class News extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            news: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
 
-    compontentDidMount() {
-        const url = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-03-04&sortBy=publishedAt&apiKey=246b9ea9ce654437acefef97d7e29302"
-   
-        fetch(url)
-        .then((response) => {
+  componentDidMount() {
+    const url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3c5c8f726f4f4d87a352e63017c68eb0';
+
+    fetch(url)
+      .then((response) => {
         return response.json();
-    })
-    .then((data) => {
+      })
+      .then((data) => {
         this.setState({
-        news: data.articles   
-    })
-    })
-    .catch((error) => console.log(error));
-    }
-    renderItems() {
-        return this.props.items.map((item) => (
-            <NewSingle key={item.id} item={item} />
-        ));
-    }
+          news: data.articles
+        })
+      })
+      .catch((error) => console.log(error));
+  }
 
- render() {
+  renderItems() {
+    return this.state.news.map((item) => (
+      <NewSingle key={item.id} item={item} />
+    ));
+  }
+
+  render() {
     return (
       <ul>
-        {this.renderItem()}
-     </ul>
+        {this.renderItems()}
+      </ul>
     );
-    }
+  }
 }
 
 export default News;
+
